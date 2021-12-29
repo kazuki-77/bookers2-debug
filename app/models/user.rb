d549803c1d1385431f98ce5bdd0eb6f7727b5a39
kazuki-77 <93814427+kazuki-77@users.noreply.github.com>
2021-12-29 02:42:22 +0000

    [finish] 応用課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,18 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  has_many :relationships, foreign_key: :follower_id
+  has_many :relationships, foreign_key: :follower_id, dependent: :destroy
   has_many :followers, through: :relationships, source: :followed #あるユーザーが中間テーブルを経由してフォローしている人全員を取ってくる
 
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :followed_id #reverse_of_relationshipsは存在しないテーブルなのでclass_name: "Relationship"でテーブルを指定している
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy #reverse_of_relationshipsは存在しないテーブルなのでclass_name: "Relationship"でテーブルを指定している
   has_many :followeds, through: :reverse_of_relationships, source: :follower #あるユーザーをフォローされる側から見た中間テーブルを経由してフォローしてくれている人を全員取ってくる
 
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: { maximum: 50}
+
+  def is_followed_by?(user)
+    reverse_of_relationships.find_by(follower_id: user.id).present?
+  end
 end
