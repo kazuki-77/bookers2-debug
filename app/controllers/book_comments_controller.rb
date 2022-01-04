c2085de3eb983f65eab02905ee1694472e79e49c
kazuki-77 <93814427+kazuki-77@users.noreply.github.com>
2022-01-04 00:20:53 +0000

    [update]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,21 @@
 class BookCommentsController < ApplicationController
 
   def create
-    @books = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params) #ログインしているユーザーのidがコメントの生成をしている
-    @book_comment.book_id = book.id #投稿された内容のidをcommentのbook_idに代入　これでカラムが全て埋まる
-    if @book_comment.save
-      redirect_to book_path(@book.id)
-    else
-      render "books/show"
-    end
+    @book_comment.book_id = @book.id #投稿された内容のidをcommentのbook_idに代入　これでカラムが全て埋まる
+    @book_comment.save
+      #redirect_to book_path(@book.id)
+    #else
+     # render "books/show"
+    #end
   end
 
   def destroy
-    BookComment.find_by(id: params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
+    book_comment.destroy
+    # redirect_to book_path(params[:book_id])
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,20 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id]) #投稿されたbookのidを取得する
-    favorite = current_user.favorites.new(book_id: book.id) #ログインしているユーザーがいいねを生成している
+    @book = Book.find(params[:book_id]) #投稿されたbookのidを取得する
+    favorite = current_user.favorites.new(book_id: @book.id) #ログインしているユーザーがいいねを生成している
     favorite.save
-    redirect_to books_path
+    #redirect_to books_path ここを消すことによって
+    #リダイレクト先がない、かつJavaScriptリクエストという状況になる
+    #そして、createアクション実行後は、create.js.erbファイルを探すようになる
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to books_path
+    #redirect_to books_path 上と同じでdestroyアクション実行後は
+    #destroy.js.erbファイルを探すようになる
   end
 
 end
